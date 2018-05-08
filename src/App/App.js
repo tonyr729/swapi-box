2bcc353b7c11f79b2a375317e448fd6817875d82
Tony Robertson <tonyr729@gmail.com>
2018-05-07 21:55:39 -0600

    Modify README and Appjs

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import Intro from './Components/Statefull/Intro/index'
+import Main from './Components/Statefull/Main/index'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
       </div>
     );
   }
