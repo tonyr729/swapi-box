40de04c4d3b1750df2e08c4428c44003f40c278b
Tony Robertson <tonyr729@gmail.com>
2018-05-11 09:54:15 -0600

    Add tests for Main component and moved cardFormat logic into Card component

--- src/Containers/Main/index.test.js
+++ src/Containers/Main/index.test.js
@@ -4,7 +4,15 @@ import APIManager from '../../Helpers/API/API Manager/apiManager';
 import Main from './index'
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
-
+jest.mock('../../Helpers/API/API Manager/apiManager', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      fetchPeople: jest.fn().mockImplementation(() => Promise.resolve(['card1', 'card2'])),
+      fetchVehicles: jest.fn().mockImplementation(() => Promise.resolve(['card1', 'card2'])),
+      fetchPlanets: jest.fn().mockImplementation(() => Promise.resolve(['card1', 'card2']))
+    };
+  });
+});
 
 describe('Main', () => {
 
@@ -17,33 +25,69 @@ describe('Main', () => {
   })
 
   describe('setPeopleData method', () => {
-    let api;
-    let main;
-
-    beforeEach(() => {
-      main = shallow(<Main />);
-      api = new APIManager();
-      api.fetchPeople = jest.fn();
-
-
-    })
-
-    it('should call fetchPeople method', async () => {
-      main.instance().setPeopleData()
-  
+    it('should call fetchPeople method of the class instance', async () => {
+      const main = shallow(<Main />);
       
-      expect(api.fetchPeople.toHaveBeenCalled())
-    
+      main.instance().setPeopleData()
+
+      expect(main.instance().api.fetchPeople).toHaveBeenCalled();     
+      
     })
 
+    it('should set state to the evaluation of the fetchPeople method', async () => {
+      const main = shallow(<Main />);
+      const expected = ['card1', 'card2']
+      
+      expect(main.state('data')).toEqual([])
+
+      Promise.resolve(main.instance().setPeopleData())
+        .then(() => main.update())
+        .then(() => expect(main.state('data')).toEqual(expected))
+    })
   })
 
-  // describe('APIManager class', () => {
+  describe('setVehicleData method', () => {
+    it('should call fetchVehicle method of the class instance', async () => {
+      const main = shallow(<Main />);
+      
+      main.instance().setVehicleData()
 
-  // })
+      expect(main.instance().api.fetchVehicles).toHaveBeenCalled();     
+      
+    })
 
-  // describe('APIManager class', () => {
+    it('should set state to the evaluation of the fetchVehicles method', async () => {
+      const main = shallow(<Main />);
+      const expected = ['card1', 'card2']
+      
+      expect(main.state('data')).toEqual([])
 
-  // })
+      Promise.resolve(main.instance().setVehicleData())
+        .then(() => main.update())
+        .then(() => expect(main.state('data')).toEqual(expected))
+    })
+  })
+
+  describe('setPlanetData method', () => {
+    it('should call fetchPlanet method of the class instance', async () => {
+      const main = shallow(<Main />);
+      
+      main.instance().setPlanetData()
+
+      expect(main.instance().api.fetchPlanets).toHaveBeenCalled();     
+      
+    })
+
+    it('should set state to the evaluation of the fetchPlanets method', async () => {
+      const main = shallow(<Main />);
+      const expected = ['card1', 'card2']
+      
+      expect(main.state('data')).toEqual([])
+
+      Promise.resolve(main.instance().setPlanetData())
+        .then(() => main.update())
+        .then(() => expect(main.state('data')).toEqual(expected))
+    })
+  })
 
 })
